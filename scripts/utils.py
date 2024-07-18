bd26e15cee5e2cdc17c6535db4c0f60435735704
ronanglee <ronan54@hotmail.com>
2024-07-18 10:29:37 +0200

    push good utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,12 +2,14 @@ import json
 import logging
 import os
 import re
+from itertools import combinations
 from pathlib import Path
 
 import numpy as np  # type: ignore
+from ase.build import add_adsorbate # type: ignore
 from ase.calculators.vasp import Vasp  # type: ignore
 from ase.db import connect  # type: ignore
-from ase.io import read  # type: ignore
+from ase.io import read, write  # type: ignore
 
 
 def check_electronic(set_nelm: int) -> int:
@@ -55,9 +57,8 @@ def check_ion(set_nsw: int) -> int:
 
 
 def synthesis_stability_run_vasp(
-    directory: os.PathLike, vasp_parameters: dict, calculation: str
-) -> bool:
-    """Run the VASP calculations.
+    directory: os.PathLike, vasp_parameters: dict, calculation: str) -> bool:
+    """Run the VASP calculations with NO solvation and dipole coorections.
 
     Args:
         directory (Path): Path to the directory where to find the initial geometry.
@@ -70,16 +71,13 @@ def synthesis_stability_run_vasp(
     converged = False
     control_ion = 0
     os.chdir(directory)
-    vasp_parameters["amin"] = 0.1
-    vasp_parameters["amix"] = 0.02
-    vasp_parameters["bmix"] = 1.0
-    if vasp_parameters["ispin"] == 2:  # Check if spin polarized
-        vasp_parameters["amix_mag"] = 0.08
-        vasp_parameters["bmix_mag"] = 1.0
     atoms = read(os.path.join(directory, "init.POSCAR"))
+    mag = magmons()
+    for atom in atoms:
+        if atom.symbol in mag.keys():
+            atom.magmom = mag[atom.symbol]
     # Begin with static calculation without dipole correction
     vasp_parameters["nelmdl"] = -8
-    vasp_parameters["ibrion"] = 2
     vasp_parameters["nsw"] = 0
     paramscopy = vasp_parameters.copy()
     calc = Vasp(**paramscopy)
@@ -92,69 +90,245 @@ def synthesis_stability_run_vasp(
         control_ion = 1
     else:
         """Check if force converged"""
-        nsw = calc.int_params["nsw"]
+        ext = "SP"
+        for f in [
+            "OSZICAR",
+            "vasp.out",
+            "INCAR",
+            "POSCAR",
+            "POSCAR",
+            "POTCAR",
+            "CONTCAR",
+            "OUTCAR",
+        ]:
+            os.system(f"cp {f} {f}.{ext}")
+        # Restart Calculation from CHGCAR include dipole correction
+        vasp_parameters["icharg"] = 1
+        for atom in atoms:
+            if atom.symbol in mag.keys():
+                atom.magmom = mag[atom.symbol]
+        vasp_parameters["ldipol"] = True
+        vasp_parameters["idipol"] = 3
+        vasp_parameters["dipol"] = atoms.get_center_of_mass(scaled=True)
+        vasp_parameters["nsw"] = 999
+        vasp_parameters["lwave"] = True
+        paramscopy = vasp_parameters.copy()
+        calc = Vasp(**paramscopy)
+        atoms.set_calculator(calc)
+        atoms.get_potential_energy()
+        """Check if a vasp calculation (eletronic self consistance) is converged"""
+        nelm = calc.int_params["nelm"]
+        control_electronic = check_electronic(nelm)
+        if control_electronic == 0:
+            print("Error: electronic scf")
+            control_ion = 1
+        else:
+            """Check if force converged"""
+            nsw = calc.int_params["nsw"]
+            control_ion = check_ion(nsw)
+            if control_ion == 1:
+                ext = "opt"
+                for f in [
+                    "OSZICAR",
+                    "vasp.out",
+                    "INCAR",
+                    "POSCAR",
+                    "POTCAR",
+                    "CONTCAR",
+                    "OUTCAR",
+                ]:
+                    os.system(f"cp {f} {f}.{ext}")
+                # Clean up
+                for f in [
+                    "CHG",
+                    "CHGCAR",
+                    "WAVECAR",
+                    "CONTCAR",
+                    "DOSCAR",
+                    "EIGENVAL",
+                    "PROCAR",
+                ]:
+                    os.remove(f)
+                    converged = True
+    # clean up
+    for f in [
+        "CHG",
+        "CHGCAR",
+        "WAVECAR",
+        "POSCAR",
+        "CONTCAR",
+        "DOSCAR",
+        "EIGENVAL",
+        "PROCAR",
+    ]:
+        os.system("rm %s" % f)
+    if converged:
+        return True
+    else:
+        print(f"{calculation} calculation did not converge.")
+        return False
+    
+def operating_stability_run_vasp(
+    directory: os.PathLike, vasp_parameters: dict, calculation: str, solvation: bool | None = False) -> bool:
+    """Run the VASP calculations.
+
+    Args:
+        directory (Path): Path to the directory where to find the initial geometry.
+        vasp_parameters (dict): Dictionary containing the VASP parameters.
+        calculation (str): Type of calculation to run.
+        solvation (bool): Whether to run the calculation with implicit solvation and dipole correction or not.
+
+    Returns:
+        bool: True if the calculation converged, False otherwise.
+    """
+    converged = False
+    control_ion = 0
+    os.chdir(directory)
+    atoms = read(os.path.join(directory, "init.POSCAR"))
+    mag = magmons()
+    for atom in atoms:
+        if atom.symbol in mag.keys():
+            atom.magmom = mag[atom.symbol]
+    control_ion = 0
+    # static calculation always at the beginning
+    vasp_parameters["istart"] = 0  # strart from being from scratch
+    vasp_parameters["icharg"] = 2  # take superposition of atomic charge density
+    vasp_parameters["nsw"] = 0
+    vasp_parameters["lcharg"] = True
+    vasp_parameters["lwave"] = False
+    vasp_parameters["isif"] = 0
+    paramscopy = vasp_parameters.copy()
+    calc1 = Vasp(**paramscopy)
+    atoms.set_calculator(calc1)
+    atoms.get_potential_energy()
+    """Check if a vasp calculation (electronic self consistance) is converged"""
+    nelm = calc1.int_params["nelm"]
+    control_electronic = check_electronic(nelm)
+    if control_electronic == 0:
+        print("Error: electronic scf")
+        control_ion = 1
+    else:
+        ext = "preSP"
+        for f in [
+            "INCAR",
+            "POSCAR",
+            "POTCAR",
+            "CONTCAR",
+            "OUTCAR",
+            "OSZICAR",
+            "vasp.out",
+        ]:
+            os.system("cp %s %s.%s" % (f, f, ext))
+    # relax and dipole correction calculation
+    atoms = read("CONTCAR")
+    for atom in atoms:
+        if atom.symbol in mag.keys():
+            atom.magmom = mag[atom.symbol]
+    vasp_parameters["istart"] = 0  # not to read WAVECAR
+    vasp_parameters["icharg"] = 1  # restrat from CHGCAR
+    vasp_parameters["ldipol"] = True
+    vasp_parameters["idipol"] = 3
+    vasp_parameters["dipol"] = atoms.get_center_of_mass(scaled=True)
+    if solvation == "implicit":
+        vasp_parameters["isif"] = 0
+        vasp_parameters["lwave"] = True
+        vasp_parameters["lcharg"] = True
+        vasp_parameters["nsw"] = 50
+    else:
+        vasp_parameters["isif"] = 0
+        vasp_parameters["lwave"] = False
+        vasp_parameters["lcharg"] = True
+        vasp_parameters["nsw"] = 50
+    paramscopy = vasp_parameters.copy()
+    calc2 = Vasp(**paramscopy)
+    atoms.set_calculator(calc2)
+    atoms.get_potential_energy()
+    """Check if a vasp calculation is converged"""
+    nelm = calc2.int_params["nelm"]
+    control_electronic = check_electronic(nelm)
+    if control_electronic == 0:
+        print("Error: electronic scf")
+        control_ion = 1
+    else:
+        """Check if force converged"""
+        nsw = calc2.int_params["nsw"]
         control_ion = check_ion(nsw)
         if control_ion == 1:
-            ext = "SP"
+            if solvation == "implicit":
+                ext = "preRDip"
+            else:
+                ext = "RDip"
             for f in [
-                "OSZICAR",
-                "vasp.out",
                 "INCAR",
-                "POSCAR",
                 "POSCAR",
                 "POTCAR",
                 "CONTCAR",
                 "OUTCAR",
+                "OSZICAR",
+                "vasp.out",
             ]:
-                os.system(f"cp {f} {f}.{ext}")
-            # Restart Calculation from CHGCAR include dipole correction
-            vasp_parameters["icharg"] = 1
-            vasp_parameters["ldipol"] = True
-            vasp_parameters["idipol"] = 3
-            vasp_parameters["dipol"] = atoms.get_center_of_mass(scaled=True)
-            vasp_parameters["nsw"] = 0
-            vasp_parameters["lwave"] = True
-            paramscopy = vasp_parameters.copy()
-            calc = Vasp(**paramscopy)
-            atoms.set_calculator(calc)
-            atoms.get_potential_energy()
-            """Check if a vasp calculation (eletronic self consistance) is converged"""
-            nelm = calc.int_params["nelm"]
-            control_electronic = check_electronic(nelm)
-            if control_electronic == 0:
-                print("Error: electronic scf")
-                control_ion = 1
-            else:
-                """Check if force converged"""
-                nsw = calc.int_params["nsw"]
-                control_ion = check_ion(nsw)
-                if control_ion == 1:
-                    ext = "opt"
-                    for f in [
-                        "OSZICAR",
-                        "vasp.out",
-                        "INCAR",
-                        "POSCAR",
-                        "POSCAR",
-                        "POTCAR",
-                        "CONTCAR",
-                        "OUTCAR",
-                        "WAVECAR",
-                    ]:
-                        os.system(f"cp {f} {f}.{ext}")
-                    # Clean up
-                    for f in [
-                        "CHG",
-                        "CHGCAR",
-                        "WAVECAR",
-                        "POSCAR",
-                        "CONTCAR",
-                        "DOSCAR",
-                        "EIGENVAL",
-                        "PROCAR",
-                    ]:
-                        os.remove(f)
-                        converged = True
+                os.system("cp %s %s.%s" % (f, f, ext))
+    # clean up
+    for f in ["DOSCAR", "EIGENVAL", "PROCAR", "vasprun.xml"]:
+        os.system("rm %s" % f)
+    control_ion = 0
+    os.system("cp CONTCAR.preRDip CONTCAR")
+    os.system("cp WAVECAR.preRDip WAVECAR")
+    # relaxation with solvation and dipole correction
+    atoms = read("CONTCAR")
+    for atom in atoms:
+        if atom.symbol in mag.keys():
+            atom.magmom = mag[atom.symbol]
+    vasp_parameters["istart"] = 1  # start from WAVECAR in vaccum
+    vasp_parameters["nsw"] = 500
+    vasp_parameters["ldipol"] = True
+    vasp_parameters["idipol"] = 3
+    vasp_parameters["dipol"] = atoms.get_center_of_mass(scaled=True)
+    vasp_parameters["isif"] = 0
+    vasp_parameters["lwave"] = True
+    vasp_parameters["lcharg"] = True
+    vasp_parameters["lsol"] = True
+    vasp_parameters["eb_k"] = 80
+    vasp_parameters["isif"] = 0
+    paramscopy = vasp_parameters.copy()
+    calc4 = Vasp(**paramscopy)
+    atoms.set_calculator(calc4)
+    atoms.get_potential_energy()
+    """Check if a vasp electronic calculation is converged"""
+    nelm = calc4.int_params["nelm"]
+    control_electronic = check_electronic(nelm)  # check electronic scf
+    if control_electronic == 0:
+        print("Error: electronic scf")
+        control_ion = 1
+    else:
+        """Check if force converged"""
+        nsw = calc4.int_params["nsw"]
+        control_ion = check_ion(nsw)
+        if control_ion == 1:
+            ext = "RDip"
+            for f in [
+                "INCAR",
+                "POSCAR",
+                "POTCAR",
+                "CONTCAR",
+                "OUTCAR",
+                "OSZICAR",
+                "vasp.out",
+            ]:
+                os.system("cp %s %s.%s" % (f, f, ext))
+                converged = True
+    # clean up
+    for f in [
+        "CHG",
+        "CHGCAR",
+        "WAVECAR",
+        "POSCAR",
+        "CONTCAR",
+        "DOSCAR",
+        "EIGENVAL",
+        "PROCAR",
+    ]:
+        os.system("rm %s" % f)
     if converged:
         return True
     else:
@@ -302,7 +476,7 @@ def read_and_write_database(outcar: os.PathLike, database: str, data: dict) -> N
     """
     data_base_folder = Path(data["base_dir"]) / "runs" / "databases"
     db = connect(data_base_folder / f"{database}.db")
-    structure = read(outcar)
+    structure = read(f'{outcar}@-1')
     db.write(structure, key_value_pairs={**data})
 
 
@@ -332,3 +506,99 @@ def run_logger(msg: str, filename: str, type_msg: str) -> None:
         logger.error(msg)
     else:
         logger.info(msg)
+
+def get_xch_structs(data: list) -> None:
+    """Get xch structures for running calculation.
+
+    Args:
+        data (dict): A dictionary containing the run parameters.
+
+    Returns:
+        None.
+    """
+    new_data = {}
+    overall_idx = 0
+    run_struc = data["run_structure"]
+    name = Path(run_struc).stem
+    metal = data["metal"]
+    base_dir = data["base_dir"]
+    skimmed_data = data.copy()
+    del skimmed_data["metal"]
+    structure = read(os.path.join(run_struc, "init.POSCAR"))
+    m_x = [atom.x for atom in structure if atom.symbol == metal]
+    m_y = [atom.y for atom in structure if atom.symbol == metal]
+    indices = {}
+    for atom in structure:
+        if atom.symbol != metal:
+            atom_x = atom.x
+            atom_y = atom.y
+            distance = list(np.sqrt((m_x - atom_x) ** 2 + (m_y - atom_y) ** 2))[0]
+            if distance < 2.2:
+                indices[atom.index] = atom.symbol
+    no_metal = structure.copy()
+    del no_metal[[atom.index for atom in structure if atom.symbol == metal]]
+    for h in range(1, len(indices) + 1):
+        no_metal_copy = no_metal.copy()
+        idicess = []
+        for atom in no_metal:
+            if atom.index in indices.keys():
+                idicess.append(atom.index)
+                if len(idicess) == h:
+                    perm = combinations(indices.keys(), h)
+                    idxx = 0
+                    combos = []
+                    for p in perm:
+                        if idxx < 5 - h:
+                            combos.append(p)
+                        idxx += 1
+                    for idx, combo in enumerate(combos):
+                        no_metal_copy = no_metal.copy()
+                        for c in combo:
+                            add_adsorbate(
+                                no_metal_copy,
+                                "H",
+                                height=1.0,
+                                position=(no_metal[c].x, no_metal[c].y),
+                            )
+                        metal_str = "M"  # so f-string wont give error
+                        run_dir = Path(
+                            os.path.join(
+                                base_dir,
+                                "runs",
+                                "operating_stability",
+                                "e_xch_implicit_solv",
+                                f"{name.replace(metal, metal_str)}",
+                                f"{h}H_config{idx+1}",
+                            )
+                        )
+                        run_dir.mkdir(exist_ok=True, parents=True)
+                        write(f"{run_dir}/init.POSCAR", no_metal_copy, format="vasp")
+                        merged_dict = {**skimmed_data.copy(), "run_dir": str(run_dir)}
+                        new_data[overall_idx] = merged_dict
+                        overall_idx += 1
+    return new_data
+
+def magmons():
+    """All ferromagnetic or anti-ferromagnetic elements 
+    and their magnetic moments multiplied
+    by 1.2 already for VASP calculations.
+    """
+    elements = {
+        "Co": 1.2, # multiplied by 1.2
+        "Ti": 1.8, # multiplied by 1.2
+        "Ni": 1.8, # multiplied by 1.2
+        "Mn": 6.0, # multiplied by 1.2 ( Mn2+ ? can be paramgentic or anti-ferromagnetic)
+        "Fe": 3.6, # multiplied by 1.2
+        "V": 3.8, # multiplied by 1.2
+        "Zr": 1.8, # multiplied by 1.2
+        "Nb": 3.2, # multiplied by 1.2
+        "Mo": 4.6, # multiplied by 1.2
+        "Ta": 3.4, # multiplied by 1.2
+        "W": 6.2, # multiplied by 1.2
+        "Sc": 2.8, # multiplied by 1.2
+        "Pt": 0.5,  # NOT multiplied by 1.2 as paramagnetic
+        "Pd": 0.5, # NOT multiplied by 1.2 as paramagnetic
+        "Cr": 3.9  # multiplied by 1.2
+        }
+
+    return elements
